7546030853b7dc4ff451ed3f33e290eb416d8a81
Adam Patterson <hello@adampatterson.ca>
2023-06-08 20:41:35 -0600

    Cleanup

--- src/WpOAuth.php
+++ src/WpOAuth.php
@@ -22,6 +22,8 @@ class WpOAuth
     private $clientRedirect;
     private $clientId;
     private $clientSecret;
+    private $state;
+    private $code;
     private $token;
     private $refreshToken;
     private $expiresIn;
@@ -42,6 +44,7 @@ class WpOAuth
         "clientRedirect",
         "clientId",
         "clientSecret",
+        "state",
         "scope",
         "response_type",
         "transient_prefix"
@@ -54,51 +57,53 @@ class WpOAuth
         "clientRedirect"     => "https://site.com/?callback=wpoauth",
         "clientId"           => "",
         "clientSecret"       => "",
+        "state"              => "random_state_string",
         "scope"              => "read offline_access",
         "response_type"      => "code",
         "expires_in"         => HOUR_IN_SECONDS - 1,
         "refresh_expires_in" => (WEEK_IN_SECONDS * 2) - 1,
         "transient_prefix"   => 'change_me'
-        "should_log"         => true,
+        "should_log"         => WP_DEBUG,
         "log_path"           => __DIR__.'/_log.php',
     ];
 
     $this->wpOAuth = new WpOAuth($wpOAuthParams);
      */
+
     public function __construct($settings = [])
     {
         // Should validate to make sure that all keys are used.
-        $this->authUrl          = $settings["authUrl"];
-        $this->tokenUrl         = $settings["tokenUrl"];
-        $this->clientRedirect   = $settings["clientRedirect"];
-        $this->clientId         = $settings["clientId"];
-        $this->clientSecret     = $settings["clientSecret"];
-        $this->scope            = $settings["scope"];
-        $this->responseType     = $settings["response_type"];
-        $this->responseStatus  = null;
+        $this->authUrl = $settings["authUrl"];
+        $this->tokenUrl = $settings["tokenUrl"];
+        $this->clientRedirect = $settings["clientRedirect"];
+        $this->clientId = $settings["clientId"];
+        $this->clientSecret = $settings["clientSecret"];
+        $this->scope = $settings["scope"];
+        $this->state = $settings["state"];
+        $this->responseType = $settings["response_type"];
+        $this->responseStatus = null;
 
-        $this->transientPrefix  = $settings["transient_prefix"];
-        
-        // Token Expiry times
-        $this->expiresIn        = $settings["expires_in"];
-        $this->refreshExpiresIn = $settings["refresh_expires_in"];
+        $this->transientPrefix = $settings["transient_prefix"];
+
+        // @todo if the settings contain token expiry then we will use it, so set it here.
 
         // Optional logging
-        $this->shouldLog = (array_key_exists('should_log', $settings) && (bool) $settings["should_log"]) ? true : false;
-        $this->logPath   = (array_key_exists('log_path',
-                $settings) && (bool) $settings["log_path"]) ? $settings["log_path"] : false;
+        $this->shouldLog = (array_key_exists('should_log', $settings) && (bool)$settings["should_log"]) ? true : false;
+        $this->logPath = (array_key_exists('log_path', $settings) && (bool)$settings["log_path"]) ? $settings["log_path"] : false;
 
         // offline_access is required for refresh tokens.
         $this->authParams = [
             "response_type" => $this->responseType,
             "scope"         => $this->scope,
+            "state"         => $this->state,
             "client_id"     => $this->clientId,
-            "redirect_uri"  => $this->clientRedirect
+            "redirect_uri"  => $this->clientRedirect,
         ];
 
         $this->tokenParams = [
             "client_id"     => $this->clientId,
             'client_secret' => $this->clientSecret,
+            "state"         => $this->state,
             'grant_type'    => 'authorization_code',
             "redirect_uri"  => $this->clientRedirect,
         ];
@@ -116,7 +121,7 @@ class WpOAuth
      */
     public function authOrNot()
     {
-        // Are we wrking with a valid token?
+        // Are we working with a valid token?
         if ($this->isTokenExpired()) {
             // The token has expired, we need to refresh
             $this->log('The token has expired, we need to refresh');
@@ -129,30 +134,31 @@ class WpOAuth
                 // Client needs to reauthenticate the app 💥
                 $this->log('Client needs to reauthenticate the app');
 //                if (current_user_can('administrator')) {
-                    $this->log('Post Token');
+                $this->log('Post Token');
 //                    $this->makeAuthLink();
-                    $this->postToken();
+                $this->postToken();
 //                }
             }
         }
 
         // Return old tokens.
-        $this->token        = $this->getToken();
+        $this->token = $this->getToken();
         $this->refreshToken = $this->getRefreshToken();
     }
 
     public function makePrefix($prefix)
     {
-        return $this->transientPrefix.'_'.$prefix;
+        return $this->transientPrefix . '_' . $prefix;
     }
 
     public function setTokens($response)
     {
-        if ($this->responseStatus) {
-            $this->token        = set_transient($this->makePrefix('access_token'), $response['access_token'],
-                $this->expiresIn);
-            $this->refreshToken = set_transient($this->makePrefix('refresh_token'), $response['refresh_token'],
-                $this->refreshExpiresIn);
+        if ($this->responseStatus == 200) {
+            // @#todo if the token expiry is set by the settings then we will use that over the response values
+            // https://developer.wordpress.org/reference/functions/set_transient/
+            $this->token = set_transient($this->makePrefix('access_token'), $response['access_token'], $response['expires_in']);
+            // @todo when we don't get a refresh token
+            $this->refreshToken = set_transient($this->makePrefix('refresh_token'), $response['refresh_token'], $response['refresh_token_expires_in');
         } else {
             if ($this->responseStatus == 400) {
                 $this->log('Invalid grant or invalid message', $response);
@@ -172,21 +178,26 @@ class WpOAuth
     {
         $code = Request::get('code', false);
 
-        if ( ! $code) {
+        if (!$code) {
             return false;
         }
 
         return true;
     }
 
+    public function authenticated(): bool
+    {
+        return !$this->isTokenExpired();
+    }
+
     public function isTokenExpired()
     {
-        return ! get_transient($this->makePrefix('access_token'));
+        return !get_transient($this->makePrefix('access_token'));
     }
 
     public function hasRefreshToken()
     {
-        return ! ! $this->getRefreshToken();
+        return !!$this->getRefreshToken();
     }
 
     public function getRefreshToken()
@@ -199,10 +210,10 @@ class WpOAuth
         return get_transient($this->makePrefix('access_token'));
     }
 
-    public function makeAuthLink()
+    public function makeAuthLink($class = null)
     {
-        if ( ! $this->isAuthenticating()) {
-            echo "<a href='{$this->getAuthUrl()}'>Authorize</a>";
+        if (!$this->isAuthenticating()) {
+            return "<a href='{$this->getAuthUrl()}' class='{$class}'>Authorize</a>";
         }
     }
 
@@ -211,7 +222,7 @@ class WpOAuth
      */
     public function getAuthUrl()
     {
-        return $this->authUrl.'?'.http_build_query($this->authParams);
+        return $this->authUrl . '?' . http_build_query($this->authParams);
     }
 
     /*
@@ -219,17 +230,40 @@ class WpOAuth
      */
     public function postToken()
     {
-        $code = Request::get('code', false);
-
-        if ( ! $code) {
+        $this->code = Request::get('code', false);
+        if (!$this->code) {
+            // Exception Missing Code
             return;
         }
 
-        $postResponse = Http::asFormParams()->post($this->tokenUrl,
-            array_merge(['code' => $code], $this->tokenParams));
+        $state = Request::get('state', false);
+        if (!$state && $state !== $this->state) {
+            // Exception Mismatched State
+            return;
+        }
 
-        $response             = $postResponse->json();
+        $params = array_merge(['code' => $this->code], $this->tokenParams);
+
+        $postResponse = Http::asFormParams()
+            ->withHeaders([
+                'Accept' => 'application/json'
+            ])
+            ->post($this->tokenUrl,
+                $params
+            );
+
+        // A status of 200 is returned even if a bad verification code is used.
+        // It's also possible that the auth API didn't return JSON.
+        // parse_str($postResponse->body(), $tokenData);
+        if ($postResponse->json() === null) {
+            $this->responseStatus = 500;
+            return;
+        }
+
+        $response = $postResponse->json();
         $this->responseStatus = $postResponse->status();
+
+//        dd($response, $postResponse->body());
 
         $this->setTokens($response);
 
@@ -247,7 +281,7 @@ class WpOAuth
             $this->tokenUrl, array_merge(['refresh_token' => $this->getRefreshToken()], $this->refreshParams)
         );
 
-        $response             = $postResponse->json();
+        $response = $postResponse->json();
         $this->responseStatus = $postResponse->status();
 
         $this->log('Exchange the authorization code for an access token', $response);
@@ -260,7 +294,7 @@ class WpOAuth
      */
     public function redirectTo($url)
     {
-        header('Location: '.$url);
+        header('Location: ' . $url);
         exit;
     }
 
@@ -289,9 +323,9 @@ class WpOAuth
 
     public function getExpiryTime($key)
     {
-        $transient = get_option('_transient_timeout_'.$key, 0);
+        $transient = get_option('_transient_timeout_' . $key, 0);
 
-        return ($transient === 0) ? 'not set' : get_option('_transient_timeout_'.$key, 0) - time();
+        return ($transient === 0) ? 'not set' : get_option('_transient_timeout_' . $key, 0) - time();
     }
 
     /**
@@ -299,8 +333,8 @@ class WpOAuth
      */
     public function writeLog($message, $data)
     {
-        if ( ! file_exists($this->logPath)) {
-            $open  = fopen($this->logPath, "a");
+        if (!file_exists($this->logPath)) {
+            $open = fopen($this->logPath, "a");
             $write = fputs($open, "<?php \r\n die; \r\n?>\r\n");
             fclose($open);
         }
@@ -311,12 +345,12 @@ class WpOAuth
         $logMessage .= "# $time\r\n$message\r\n\r\n";
 
         if (is_array($data)) {
-            $logMessage .= "# Data:\r\n".print_r($data, true)."\r\n";
+            $logMessage .= "# Data:\r\n" . print_r($data, true) . "\r\n";
         }
 
-        $logMessage .= "# Debug:\r\n".print_r($this->getDebugDetals(), true)."\r\n";
+        $logMessage .= "# Debug:\r\n" . print_r($this->getDebugDetals(), true) . "\r\n";
 
-        $open  = fopen($this->logPath, "a");
+        $open = fopen($this->logPath, "a");
         $write = fputs($open, $logMessage);
         fclose($open);
     }
